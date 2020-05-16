9c905998d25e7e004b41aa4a6735507683dc7197
Brian Heylin <git@heylin.nl>
2020-05-17 00:23:36 +0200

    Parser now returns an result for each line of text given.

--- hullatodo_txt/src/lib.rs
+++ hullatodo_txt/src/lib.rs
@@ -14,9 +14,10 @@ mod tests {
     #[test]
     fn simplest() {
         let todos = parse("this is a todo");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("this is a todo", todos[0].text);
-        assert_eq!(false, todos[0].is_completed);
+        assert_eq!("this is a todo", first.text);
+        assert_eq!(false, first.is_completed);
     }
 
     #[test]
@@ -26,8 +27,10 @@ mod tests {
             this is the second todo"
         );
         assert_eq!(2, todos.len());
-        assert_eq!("this is the first todo", todos[0].text);
-        assert_eq!("this is the second todo", todos[1].text);
+        let first = todos[0].as_ref().unwrap();
+        let second = todos[1].as_ref().unwrap();
+        assert_eq!("this is the first todo", first.text);
+        assert_eq!("this is the second todo", second.text);
 
         let todos = parse(
             "this is the first todo\n\
@@ -35,158 +38,186 @@ mod tests {
             2020-05-18 this is the +third todo");
         
         assert_eq!(3, todos.len());
-        assert_eq!("this is the first todo", todos[0].text);
-        assert_eq!("this is the @second todo", todos[1].text);
-        assert_eq!("this is the +third todo", todos[2].text);
+        let first = todos[0].as_ref().unwrap();
+        let second = todos[1].as_ref().unwrap();
+        let third = todos[2].as_ref().unwrap();
+        assert_eq!("this is the first todo", first.text);
+        assert_eq!("this is the @second todo", second.text);
+        assert_eq!("this is the +third todo", third.text);
 
-        assert_eq!(false, todos[0].is_completed);
-        assert_eq!(true, todos[1].is_completed);
-        assert_eq!(false, todos[2].is_completed);
+        assert_eq!(false, first.is_completed);
+        assert_eq!(true, second.is_completed);
+        assert_eq!(false, third.is_completed);
 
-        assert_eq!(None, todos[0].date_completed);
-        assert_eq!(Some(Date(2020, 5, 17)), todos[1].date_completed);
-        assert_eq!(Some(Date(2020, 5, 18)), todos[2].date_completed);
+        assert_eq!(None, first.date_completed);
+        assert_eq!(Some(Date(2020, 5, 17)), second.date_completed);
+        assert_eq!(Some(Date(2020, 5, 18)), third.date_completed);
 
-        assert_eq!(0, todos[0].context_tags.len());
-        assert_eq!(0, todos[0].project_tags.len());
-        assert_eq!(1, todos[1].context_tags.len());
-        assert_eq!(0, todos[1].project_tags.len());
-        assert_eq!("second", todos[1].context_tags[0]);
-        assert_eq!(0, todos[2].context_tags.len());
-        assert_eq!(1, todos[2].project_tags.len());
-        assert_eq!("third", todos[2].project_tags[0]);
+        assert_eq!(0, first.context_tags.len());
+        assert_eq!(0, first.project_tags.len());
+        assert_eq!(1, second.context_tags.len());
+        assert_eq!(0, second.project_tags.len());
+        assert_eq!("second", second.context_tags[0]);
+        assert_eq!(0, third.context_tags.len());
+        assert_eq!(1, third.project_tags.len());
+        assert_eq!("third", third.project_tags[0]);
     }
 
     #[test]
     fn completeness() {
         let todos = parse("x this is a todo");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("this is a todo", todos[0].text);
-        assert_eq!(true, todos[0].is_completed);
+        assert_eq!("this is a todo", first.text);
+        assert_eq!(true, first.is_completed);
 
         let todos = parse("xylophone lesson");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("xylophone lesson", todos[0].text);
-        assert_eq!(false, todos[0].is_completed);
+        assert_eq!("xylophone lesson", first.text);
+        assert_eq!(false, first.is_completed);
 
         let todos = parse("X 2012-01-01 Make resolutions");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("X 2012-01-01 Make resolutions", todos[0].text);
-        assert_eq!(false, todos[0].is_completed);
+        assert_eq!("X 2012-01-01 Make resolutions", first.text);
+        assert_eq!(false, first.is_completed);
 
         let todos = parse("(A) x Find ticket prices");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("x Find ticket prices", todos[0].text);
-        assert_eq!(false, todos[0].is_completed);
+        assert_eq!("x Find ticket prices", first.text);
+        assert_eq!(false, first.is_completed);
     }
 
     #[test]
     fn priority() {
         let todos = parse("(A) this has the highest priority");
-        assert_eq!("this has the highest priority", todos[0].text);
-        assert_eq!(Some(0), todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("this has the highest priority", first.text);
+        assert_eq!(Some(0), first.priority);
 
         let todos = parse("(Z) this has the lowest priority");
-        assert_eq!("this has the lowest priority", todos[0].text);
-        assert_eq!(Some(25), todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("this has the lowest priority", first.text);
+        assert_eq!(Some(25), first.priority);
 
         let todos = parse("(+) this is a todo");
-        assert_eq!("(+) this is a todo", todos[0].text);
-        assert_eq!(None, todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("(+) this is a todo", first.text);
+        assert_eq!(None, first.priority);
 
         let todos = parse("Really gotta call Mom (A) @phone @someday");
-        assert_eq!("Really gotta call Mom (A) @phone @someday", todos[0].text);
-        assert_eq!(None, todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("Really gotta call Mom (A) @phone @someday", first.text);
+        assert_eq!(None, first.priority);
 
         let todos = parse("(b) Get back to the boss");
-        assert_eq!("(b) Get back to the boss", todos[0].text);
-        assert_eq!(None, todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("(b) Get back to the boss", first.text);
+        assert_eq!(None, first.priority);
 
         let todos = parse("(B)->Submit TPS report");
-        assert_eq!("(B)->Submit TPS report", todos[0].text);
-        assert_eq!(None, todos[0].priority);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("(B)->Submit TPS report", first.text);
+        assert_eq!(None, first.priority);
     }
 
     #[test]
     fn tags() {
         let todos = parse("(A) Call Mom +Family +PeaceLoveAndHappiness @iphone @phone");
-        assert_eq!("Call Mom +Family +PeaceLoveAndHappiness @iphone @phone", todos[0].text);
-        assert_eq!(Some(0), todos[0].priority);
-        assert_eq!(2, todos[0].context_tags.len());
-        assert_eq!("Family", todos[0].project_tags[0]);
-        assert_eq!("PeaceLoveAndHappiness", todos[0].project_tags[1]);
-        assert_eq!("iphone", todos[0].context_tags[0]);
-        assert_eq!("phone", todos[0].context_tags[1]);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("Call Mom +Family +PeaceLoveAndHappiness @iphone @phone", first.text);
+        assert_eq!(Some(0), first.priority);
+        assert_eq!(2, first.context_tags.len());
+        assert_eq!("Family", first.project_tags[0]);
+        assert_eq!("PeaceLoveAndHappiness", first.project_tags[1]);
+        assert_eq!("iphone", first.context_tags[0]);
+        assert_eq!("phone", first.context_tags[1]);
 
         let todos = parse("Email SoAndSo at soandso@example.com");
-        assert_eq!("Email SoAndSo at soandso@example.com", todos[0].text);
-        assert_eq!(0, todos[0].context_tags.len());
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("Email SoAndSo at soandso@example.com", first.text);
+        assert_eq!(0, first.context_tags.len());
 
         let todos = parse("Learn how to add 2+2");
-        assert_eq!("Learn how to add 2+2", todos[0].text);
-        assert_eq!(0, todos[0].project_tags.len());
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("Learn how to add 2+2", first.text);
+        assert_eq!(0, first.project_tags.len());
 
         let todos = parse("Do Unicode tags work @ハラトド do they?");
-        assert_eq!("Do Unicode tags work @ハラトド do they?", todos[0].text);
-        assert_eq!(1, todos[0].context_tags.len());
-        assert_eq!("ハラトド", todos[0].context_tags[0]);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("Do Unicode tags work @ハラトド do they?", first.text);
+        assert_eq!(1, first.context_tags.len());
+        assert_eq!("ハラトド", first.context_tags[0]);
     }
 
     #[test]
     fn pairs() {
         let todos = parse("not-a-pair:this-is-just-text");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("not-a-pair:this-is-just-text", todos[0].text);
-        assert_eq!(0, todos[0].pair_tags.len());
+        assert_eq!("not-a-pair:this-is-just-text", first.text);
+        assert_eq!(0, first.pair_tags.len());
 
         let todos = parse("message pair-key:pair-value");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("message pair-key:pair-value", todos[0].text);
-        assert_eq!(1, todos[0].pair_tags.len());
-        assert_eq!("pair-key", todos[0].pair_tags[0].key);
-        assert_eq!("pair-value", todos[0].pair_tags[0].value);
+        assert_eq!("message pair-key:pair-value", first.text);
+        assert_eq!(1, first.pair_tags.len());
+        assert_eq!("pair-key", first.pair_tags[0].key);
+        assert_eq!("pair-value", first.pair_tags[0].value);
 
         let todos = parse("pre-message pair-key:pair-value post-message");
+        let first = todos[0].as_ref().unwrap();
         assert_eq!(1, todos.len());
-        assert_eq!("pre-message pair-key:pair-value post-message", todos[0].text);
-        assert_eq!(1, todos[0].pair_tags.len());
-        assert_eq!("pair-key", todos[0].pair_tags[0].key);
-        assert_eq!("pair-value", todos[0].pair_tags[0].value);
+        assert_eq!("pre-message pair-key:pair-value post-message", first.text);
+        assert_eq!(1, first.pair_tags.len());
+        assert_eq!("pair-key", first.pair_tags[0].key);
+        assert_eq!("pair-value", first.pair_tags[0].value);
 
         let todos = parse("pre-message keyA:valueA keyB:valueB post-message");
-        assert_eq!("pre-message keyA:valueA keyB:valueB post-message", todos[0].text);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("pre-message keyA:valueA keyB:valueB post-message", first.text);
         assert_eq!(1, todos.len());
-        assert_eq!(2, todos[0].pair_tags.len());
-        assert_eq!("keyA", todos[0].pair_tags[0].key);
-        assert_eq!("valueA", todos[0].pair_tags[0].value);
-        assert_eq!("keyB", todos[0].pair_tags[1].key);
-        assert_eq!("valueB", todos[0].pair_tags[1].value);
+        assert_eq!(2, first.pair_tags.len());
+        assert_eq!("keyA", first.pair_tags[0].key);
+        assert_eq!("valueA", first.pair_tags[0].value);
+        assert_eq!("keyB", first.pair_tags[1].key);
+        assert_eq!("valueB", first.pair_tags[1].value);
 
         // duplicate pairs should be handled by the application layer
         // it is not the parsers responsibility to destroy data
         let todos = parse("message keyA:valueA keyA:valueA");
-        assert_eq!("message keyA:valueA keyA:valueA", todos[0].text);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("message keyA:valueA keyA:valueA", first.text);
         assert_eq!(1, todos.len());
-        assert_eq!(2, todos[0].pair_tags.len());
+        assert_eq!(2, first.pair_tags.len());
     }
 
     #[test]
     fn dates() {
         let todos = parse("This is a todo without any dates");
-        assert_eq!("This is a todo without any dates", todos[0].text);
-        assert_eq!(None, todos[0].date_completed);
-        assert_eq!(None, todos[0].date_creation);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("This is a todo without any dates", first.text);
+        assert_eq!(None, first.date_completed);
+        assert_eq!(None, first.date_creation);
 
         let todos = parse("2020-05-16 This is a todo with a completion date");
-        assert_eq!("This is a todo with a completion date", todos[0].text);
-        assert_eq!(Some(Date(2020, 5, 16)), todos[0].date_completed);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("This is a todo with a completion date", first.text);
+        assert_eq!(Some(Date(2020, 5, 16)), first.date_completed);
 
         let todos = parse("2020-05-16 2020-04-12 This is a todo with a completion and creation date");
-        assert_eq!("This is a todo with a completion and creation date", todos[0].text);
-        assert_eq!(Some(Date(2020, 5, 16)), todos[0].date_completed);
-        assert_eq!(Some(Date(2020, 4, 12)), todos[0].date_creation);
+        let first = todos[0].as_ref().unwrap();
+        assert_eq!("This is a todo with a completion and creation date", first.text);
+        assert_eq!(Some(Date(2020, 5, 16)), first.date_completed);
+        assert_eq!(Some(Date(2020, 4, 12)), first.date_creation);
     }
 }
+
+use std::error;
+use std::fmt;
 
 use pest::Parser;
 
@@ -207,6 +238,22 @@ pub struct PairTag<'a> {
     value: &'a str
 }
 
+#[derive(Debug, Clone)]
+pub struct ParseError;
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+impl error::Error for ParseError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+
 #[derive(Default)]
 pub struct Todo<'a> {
     pub is_completed: bool,
@@ -219,17 +266,17 @@ pub struct Todo<'a> {
     pub pair_tags: Vec<PairTag<'a>>
 }
 
-pub fn parse(text: &str) -> Vec<Todo> {
+pub fn parse(text: &str) -> Vec<Result<Todo, ParseError>> {
     text.lines()
-        .filter_map(|line| {
+        .map(|line| {
             let result = todotxt::Parser::parse(todotxt::Rule::entry, line);
                 
             if result.is_err() {
                 // todo: error handling!!
-                None
+                Err(ParseError {})
             } else {
                 let entry_pair = result.unwrap().next().unwrap();
-                Some(parse_entry(entry_pair))
+                Ok(parse_entry(entry_pair))
             }
         })
         .collect()
